2d27a999722600f36e780c1ba8696df14a071a81
Dean Martin <deanmar@outlook.com>
2017-08-21 21:55:52 +0200

    Updating of debug example to include opening a serial connection

--- src/examples/DebugAssist/Basic/Basic.ino
+++ src/examples/DebugAssist/Basic/Basic.ino
@@ -5,10 +5,12 @@
 DebugAssist debugger;
 
 void setup() {
+  Serial.begin(9600);
   debugger.initialize(DEBUG);
 }
 
 void loop() {
   debugger.printLog("Append message to serial only when debug flag is true.");
   debugger.printLogLn("Append message to new line on serial only when debug flag is true.");
+  delay(1000);
 }
